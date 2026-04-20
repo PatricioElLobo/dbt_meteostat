0a58d95b441de796cd469578c511e5a399017629
Erick Burgueño Salas <erickburguenosalas@MacBook-Pro-5.local>
2026-04-20 11:43:06 +0200

    northwind.4

--- models/northwind/staging_orders.sql
+++ models/northwind/staging_orders.sql
@@ -12,7 +12,7 @@ SELECT
     ,shipvia AS ship_via
 	,freight
 	,shipname AS ship_name
-	,shipadress AS ship_address
+	,shipaddress AS ship_address
     ,shipcity AS ship_city
 	,shipregion AS ship_region
 	,shippostalcode AS ship_postalcode
